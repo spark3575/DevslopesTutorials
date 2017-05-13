e7aad6b413aa043153fa960f4b01717de2855b65
Shin Park <shinpark@shins-mac-mini.home>
2017-05-13 08:32:57 -0500

    Objective-C Storyboards, IBOutlets & custom table view cells

---
+++ DevslopesTutorials/VideoCell.h
@@ -0,0 +1,13 @@
+//
+//  VideoCell.h
+//  DevslopesTutorials
+//
+//  Created by Shin Park on 5/13/17.
+//  Copyright © 2017 shinDev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface VideoCell : UITableViewCell
+
+@end
