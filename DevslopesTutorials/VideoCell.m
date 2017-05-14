6156aa70bc9e2037d1446272eaf54854babba63c
Shin Park <shinpark@shins-mac-mini.home>
2017-05-13 22:39:49 -0500

    IBOutlets & showing data in cells

--- DevslopesTutorials/VideoCell.m
+++ DevslopesTutorials/VideoCell.m
@@ -7,6 +7,7 @@
 //
 
 #import "VideoCell.h"
+#import "Video.h"
 
 @interface VideoCell()
 
@@ -29,10 +30,13 @@
     self.layer.shadowOffset = CGSizeMake(0.0, 2.0);
 }
 
-- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
-    [super setSelected:selected animated:animated];
-
-    // Configure the view for the selected state
+-(void)updateUI:(nonnull Video*)video {
+    self.titleLbl.text = video.videoTitle;
+    self.descLbl.text = video.videoDescription;
+    NSLog(@"VID: %@",video.videoDescription);
+    
+    UIImage *image = [UIImage imageWithData:[NSData dataWithContentsOfURL:[NSURL URLWithString:video.thumbnailUrl]]];
+    self.thumbImg.image = image;
 }
 
 @end
--- DevslopesTutorials/ViewController.m
+++ DevslopesTutorials/ViewController.m
@@ -71,6 +71,10 @@
 
 -(void)tableView:(UITableView *)tableView willDisplayCell:(UITableViewCell *)cell forRowAtIndexPath:(NSIndexPath *)indexPath {
     
+    Video *video = [self.videoList objectAtIndex:indexPath.row];
+    VideoCell *vidCell = (VideoCell*)cell;
+    [vidCell updateUI:video];
+    
 }
 
 -(void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
